b0388ea498a44299396cc7a52b2b457645a70d38
Nikolay Cherkezishvili <ncherkezishvili@nxsystemsinc.com>
2015-04-09 20:49:55 +0100

    json test cases added

---
+++ docs/test-cases.js
@@ -0,0 +1,29 @@
+// test case 1
+
+var coke = {
+  name: 'Coke',
+  volumes: [
+    {
+      volume: '330ml',
+      price: 2
+    },
+    {
+      volume: '500ml',
+      price: 3
+    }
+  ]
+};
+
+var pizza = {
+  name: 'Margherita',
+  sizes: [
+    {
+      size: '9 inches',
+      price: 4
+    },
+    {
+      size: '12 inches',
+      price: 5
+    }
+  ]
+};
